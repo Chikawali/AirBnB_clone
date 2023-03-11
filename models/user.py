3ae1fc4b98ee066abaec5fd40b70ba2c73fd512b
black-jamesbond <kshedu197@gmail.com>
2023-03-10 22:14:18 -0600

    fixed update

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from models.basemodel import BaseModel
+
+class User(BaseModel):
+    """Represent a user.
+
+    Attribute:
+        email(str): the email of the user.
+        password (str): the password of the user.
+        first_name (str): the first name of user.
+        last_name(str) : the last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
