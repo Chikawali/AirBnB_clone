8de9130264f05616369d54fb2cc261a3297d9790
black-jamesbond <104460484+black-jamesbond@users.noreply.github.com>
2023-03-13 00:51:48 +0100

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-#import sys
-#sys.path.append('C:/Users/kshed/OneDrive/Desktop/programming/AirBnB_clone/models')
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
--- models/review.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('C:/Users/kshed/OneDrive/Desktop/programming/AirBnB_clone/models')
-from base_model import BaseModel
-
-class Review(BaseModel):
-    
-    place_id = "" #Place.id
-    user_id = "" #User.id
-    text = ""
